7b37bf221baf5857d12d0163b870447ceb8a0dfb
Georgy Ivanov <ivanov.georg@gmail.com>
2011-10-09 12:33:15 +0400

    decision stump based boosting

--- myboost.R
+++ myboost.R
@@ -1,5 +1,6 @@
+require(rpart)
 
-learn<-function(features, classes, weights) {
+my_learn<-function(features, classes, weights) {
   data<-features
   label=classes[[1]]  
  
@@ -10,13 +11,16 @@ learn<-function(features, classes, weights) {
     c0<-length(bool_classes[bool_classes %in% F])
     local_weights=weights*(bool_classes*(c0-c1) + c1)/(c1+c0)/2;
 
-    model<-lm(classes~.-classes, data, weights=local_weights)  
-   
-  return(model)
+    # model<-glm(classes~.-classes, data, weights=local_weights, family=binomial)
+    cntrl<-rpart.control(maxdepth = 1 , minsplit=length(bool_classes), maxsurrogate = 0, usesurrogate=0, maxcompete = 1,cp = 0, xval = 0)
+    model<-rpart(classes~.-classes, data, weights=local_weights, control = cntrl)
+
+  return(list(model,local_weights))
 }
 
-predict<-function(model, features) {
-  result<-(predict.lm(model, features)>0.5)
+my_predict<-function(model, features) {
+  # result<-(predict.glm(model, features) > 0.5)
+  result<-(predict(model, features) > 0.5)
   return(result)
 }
 
@@ -25,10 +29,14 @@ boost_learn<-function(features, classes, count) {
   weights<-rep(1/length(classes), length(classes))
   for(i in 1:count) {
     step=list()
-    model<-learn(features, classes, weights)
+    model_plus<-my_learn(features, classes, weights)
+    model<-model_plus[[1]]
     step[[1]]<-model
-    res<-predict(model, features)
-    error<-sum(weights*((res>0.5)!=classes))/sum(weights)
+    res<-my_predict(model, features)
+
+    local_weights<-model_plus[[2]]
+    error<-sum(local_weights*((res>0.5)!=classes))/sum(local_weights)
+    print(error)
     factor=0.5*log((1-error)/error)
     step[[2]]<-factor
     if (factor<0) {
@@ -44,7 +52,7 @@ boost_predict<-function(steps, features) {
   res<-rep(0,length(features[,1]))
   weight<-0
   for (step in steps) {
-    res<-res+step[[2]]*predict(step[[1]], features)
+    res<-res+step[[2]]*my_predict(step[[1]], features)
     weight<-weight+step[[2]]
   }
   return(res/weight)
